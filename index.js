361f47335b97e3f349b33581ec25beaa8d509237
Kenta Saito <kenta.saito@sagittar.org>
2023-09-11 17:22:52 +0900

    2023-09-11 17:05:52

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const userNames = {
-	ExamplesColor: 'kentasaito',
-	ExamplesGraphicMap: 'kentasaito',
-	ExamplesImageMap: 'kentasaito',
-	ExamplesInput: 'kentasaito',
-	ExamplesQueue: 'kentasaito',
-	ExamplesSound: 'kentasaito',
-	ExamplesSprite: 'kentasaito',
-	ExamplesState: 'kentasaito',
-	ExamplesTextMap: 'kentasaito',
-	WelcomeTo8ppoi: 'kentasaito',
+	ExamplesColor: '8ppoi',
+	ExamplesGraphicMap: '8ppoi',
+	ExamplesImageMap: '8ppoi',
+	ExamplesInput: '8ppoi',
+	ExamplesQueue: '8ppoi',
+	ExamplesSound: '8ppoi',
+	ExamplesSprite: '8ppoi',
+	ExamplesState: '8ppoi',
+	ExamplesTextMap: '8ppoi',
+	WelcomeTo8ppoi: '8ppoi',
 };
 
 import { render } from "https://deno.land/x/gfm/mod.ts";
@@ -35,7 +35,10 @@ router.get('/:cartridgeName', async context => {
 	const cartridgeName = context.params.cartridgeName.replace(/#.*/, '');
 	const decoder = new TextDecoder('utf-8');
 	let decoded = decoder.decode(Deno.readFileSync('./index.html'));
-	decoded += decoder.decode(Deno.readFileSync('./download.html')).replace(/#download#/, `https://github.com/${userNames[cartridgeName]}/${cartridgeName}/archive/refs/heads/master.zip`);
+	decoded += decoder.decode(Deno.readFileSync('./download.html'))
+		.replace(/#repositoryUrl#/, `https://github.com/${userNames[cartridgeName]}/${cartridgeName}`)
+		.replace(/#download#/, `https://github.com/${userNames[cartridgeName]}/${cartridgeName}/archive/refs/heads/master.zip`)
+	;
 	decoded += decoder.decode(Deno.readFileSync('./description.html')).replace(/#description#/, render(decoder.decode(Deno.readFileSync(`./8ppoi-dev/src/cartridges/${cartridgeName}/README.md`))));
 	context.response.body = decoded;
 })
